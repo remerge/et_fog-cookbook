db1008c1ef540ee6420b134614f00fc842fa93a5
Eric Herot <eric.github@herot.com>
2013-11-25 14:20:20 -0500

    First rev

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'et_fog'
+maintainer       'EverTrue, Inc.'
+maintainer_email 'eric.herot@evertrue.com'
+license          'All rights reserved'
+description      'Installs/Configures the Fog ruby library'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'build-essential'
---
+++ recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: et_fog
+# Recipe:: default
+#
+# Copyright (C) 2013 EverTrue, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node.set[:build_essential][:compiletime] = true
+include_recipe 'build-essential'
+
+# Dependencies required by nokogiri (for fog)
+%w(libxslt-dev libxml2-dev).each do |pkg|
+  c_pkg = package(pkg)
+  c_pkg.run_action(:install)
+end
+
+
+# TODO: Remove this once the gem_hell cookbook is ready to roll
+g = chef_gem "fog" do
+  action :nothing
+end
+
+g.run_action(:install)
