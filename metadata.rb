a69939d0f9bc879c156704a815b77274ae4ac41d
Jeff Byrnes <jeff@evertrue.com>
2014-07-17 17:44:01 -0400

    Update to Apache 2.0 license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'et_fog'
 maintainer       'EverTrue, Inc.'
-maintainer_email 'eric.herot@evertrue.com'
-license          'All rights reserved'
+maintainer_email 'devops@evertrue.com'
+license          'Apache v2.0'
 description      'Installs/Configures the Fog ruby library'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.5'
