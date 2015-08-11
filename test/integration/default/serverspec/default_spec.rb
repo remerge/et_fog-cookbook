786491884033a4726aca7fef598bea5e33f3e6a3
Eric Herot <eric.github@herot.com>
2015-08-11 15:43:38 -0400

    rspec3 test mode

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe 'Gem Installations' do
   describe command('/opt/chef/embedded/bin/gem specification unf') do
-    it { should return_exit_status 0 }
+    its(:exit_status) { should eq 0 }
     its(:stdout) { should match(/^name: unf$/) }
   end
 
   describe command('/opt/chef/embedded/bin/gem specification fog') do
-    it { should return_exit_status 0 }
+    its(:exit_status) { should eq 0 }
     its(:stdout) { should match(/^name: fog$/) }
   end
 end
