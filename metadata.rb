f7d968f176189c7ef401034dbcb903f1e4037c18
Jeff Byrnes <thejeffbyrnes@gmail.com>
2015-11-18 18:02:54 -0500

    Add source & issues URLs to metadata
    Makes Supermarket nicer

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Installs/Configures the Fog ruby library'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.2'
 
+source_url 'https://github.com/evertrue/et_fog-cookbook/' if respond_to?(:source_url)
+issues_url 'https://github.com/evertrue/et_fog-cookbook/issues' if respond_to?(:issues_url)
+
 supports 'ubuntu', '= 14.04'
 
 depends 'build-essential', '~> 2.0'
