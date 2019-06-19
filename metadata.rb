e88ee32be921cb812f343d44ed6d568fac0c6307
VOvchinnikov <37372921+VOvchinnikov@users.noreply.github.com>
2019-06-19 20:51:33 +0200

    Unpin chef_version, ubuntu vesion

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache v2.0'
 description      'Installs/Configures the Fog ruby library'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '4.0.1'
+version          '4.1.0'
 
 source_url 'https://github.com/evertrue/et_fog-cookbook/' if respond_to?(:source_url)
 issues_url 'https://github.com/evertrue/et_fog-cookbook/issues' if respond_to?(:issues_url)
 
-supports 'ubuntu', '= 14.04'
+supports 'ubuntu'
 
-chef_version '~> 12.10'
+chef_version '>= 12.10'
 
 depends 'build-essential', '>= 2.0'
